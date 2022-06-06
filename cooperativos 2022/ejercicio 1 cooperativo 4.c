30905e0786a2dec8dd32bfdb840f06318fa9daf2
Antonio  Garcia Rodriguez <100147424+agr320@users.noreply.github.com>
2022-06-06 18:46:27 +0200

    Add files via upload

---
+++ cooperativos 2022/ejercicio 1 cooperativo 4.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <ctype.h>
+#include <math.h>
+
+int main(){
+	char c;
+	int i,n;
+	float suma;
+
+	do{	system("cls");
+		printf("SUMA DE NUMEROS IMPARES MULTIPLOS DE 3\n");
+		printf("======================================\n\n");
+		do{	printf("Introduzca limite superior: ");
+			scanf(" %d", &n);
+		}while(n<0);
+		i=3;
+		suma=0;
+		while(i<=n){
+			suma+=i;
+			i+=6;
+		}
+		printf("\nLa suma vale: %.0f",suma);
+		printf("\n\nDesea efectuar una nueva operacion (s/n)? ");
+		c=toupper(getch());
+	}while (c!='N');
+	return 0;
+}
